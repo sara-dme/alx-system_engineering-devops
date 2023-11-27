90de53e892798f15c383a3e1594075210b8ff674
sara-dme <s.daoumy@gmail.com>
2023-11-27 00:24:58 -0800

    fix some changes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
 #install flask from pip3
+pachage { 'python3':
+  ensure   => installed,
+}
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
 package { 'flask':
   ensure   => '2.1.0',
-  provider => pip3
+  provider => 'pip3'
 }
