de15b8367e4a16260898b46fe43c82451bf59d5a
sara-dme <s.daoumy@gmail.com>
2024-01-16 13:04:39 -0800

    fix task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,15 +4,14 @@ contain the function that returns the number of subscribers
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """ function that queries the Reddit API and returns
     the number of subscribers """
-    if not subreddit or type(subreddit) is not str:
-        return 0
-    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64;'
-               'rv:121.0) Gecko/20100101 '
-               'Firefox/121.0 Herring/97.1.5770.71'}
+    url = "https://www.reddit.com/r/{0}/about.json".format(str(subreddit))
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+               'AppleWebKit/537.36 (KHTML, like Gecko) '
+               'Chrome/120.0.0.0 Safari/537.36'}
     req = requests.get(url, headers=headers)
     if req.status_code == 200:
         return req.json().get("data").get("subscribers")
