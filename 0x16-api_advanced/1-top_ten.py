85944f4ce659e578e1d784758840506b682fb587
sara-dme <s.daoumy@gmail.com>
2024-01-16 13:12:50 -0800

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,19 +5,20 @@ contain thei function that prints the titles of the first
 """
 import requests
 
-def top_ten(subreddit)
+def top_ten(subreddit):
     """ function that prints the 10 first post listed
     for a given subscribers """
-    if not subreddit or type(subreddit) is not str:
-        return 0
-    url = 'http://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': ''}
-    req = requests.get(url, allow_redirects=False, headers=headers)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; '
+               'rv:121.0) Gecko/20100101 Firefox/121.8'}
+    params = { "limit": 10 }
+    req = requests.get(url, allow_redirects=False, params=params,
+                       headers=headers)
     if req.status_code == 200:
         data = req.json().get('data')
         article = data.get("children")
         for a in article:
             data = a.get("data")
             print(data.get("title"))
-        else:
+    else:
             print(None)
