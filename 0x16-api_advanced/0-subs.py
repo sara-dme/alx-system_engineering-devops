a5f80d49235bbb7ebd617abae17c5b25f28b262f
sara-dme <s.daoumy@gmail.com>
2024-01-16 12:56:05 -0800

    fix task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,9 @@ def number_of_subscribers(subreddit):
     if not subreddit or type(subreddit) is not str:
         return 0
     url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win; x64)'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64;'
+               'rv:121.0) Gecko/20100101 '
+               'Firefox/121.0 Herring/97.1.5770.71'}
     req = requests.get(url, headers=headers)
     if req.status_code == 200:
         return req.json().get("data").get("subscribers")
