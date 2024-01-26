9cdc88b81f59efd03ab4fdbb51847711a8880973
sara-dme <s.daoumy@gmail.com>
2024-01-25 23:35:25 -0800

    fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,10 +2,10 @@
 
 exec {'fix-unlimit':
   command  => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
-  path     => '/usr/local/bin/:/bin/',
+  path     => '/usr/local/bin/:/bin/'
 }
 
 exec {'nginx-restart':
   command  => '/etc/init.d/nginx restart',
-  path     => '/etc/init.d',
+  path     => '/etc/init.d'
 }
