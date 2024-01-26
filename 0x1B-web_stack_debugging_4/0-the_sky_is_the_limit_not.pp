2e5c6b06f5479e734789a611876e911f4d5c79e1
sara-dme <s.daoumy@gmail.com>
2024-01-25 23:08:17 -0800

    task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # increase the UNLIMIT of the file
 exec { 'fix-unlimit':
-  command  => "sed -i 's/^UNLIMIT=.*/ULIMIT=\"-n 15000\"/' /etc/default/nginx",
-  provider => 'shell',
+  command  => '/bin/sed -i "s/15/4096" /etc/default/nginx',
+  path => 'usr/local/bin/:/bin/',
 }
 
 -> exec { 'restart':
